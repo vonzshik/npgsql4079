971f4c150c7ea59f23d87a821731c7a630077ea1
Nikita Kazmin <nkazmin@syntellect.ru>
2021-11-01 11:07:58 +0300

    First commit

---
+++ npgsql4079/Program.cs
@@ -0,0 +1,27 @@
+﻿using Npgsql;
+using System;
+
+namespace npgsql4079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = new NpgsqlConnection("Host=localhost; Database=postgres; User ID=postgres; Password=Master1234"))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("Successfully connected");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                Console.WriteLine("Press any key...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
